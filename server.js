cf247e82c4b8a92bddc5ae33e8f8e897386ce950
Colby Cooke <colby.n.cooke@vanderbilt.edu>
2019-07-04 21:30:00 -0500

    another heorku push

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
-const users = require("./routes/api/users")
-const news = require('./routes/api/news');
+//const users = require("./routes/api/users")
+//const news = require('./routes/api/news');
 const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+//const server = require('http').Server(app);
+//const io = require('socket.io')(server);
 require('dotenv').config({ path: '.env' })
 const NewsAPI = require('newsapi');
 const Pusher = require('pusher');
@@ -204,9 +204,9 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Routes
-app.use("/api/users", users);
+app.use(routes);
 //app.use("/api/news", news);
 //app.use("/api/messages", messages);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
-server.listen(port, () => console.log(`Server up and running on port ${port} !`));
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
