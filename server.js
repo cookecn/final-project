75d2d931734f6b740eeae821fcfc7b0e22e4120b
Colby Cooke <colby.n.cooke@vanderbilt.edu>
2019-07-07 19:44:21 -0500

    recent typo with express.static

--- server.js
+++ server.js
@@ -218,7 +218,7 @@ var reqTimer = setTimeout(function wakeUp() {
   return reqTimer = setTimeout(wakeUp, 1200000);
 }, 1200000);
 
-app.use(Express.static(path.join(__dirname, '/client/build')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.get('*', (req, res) => {
 	res.sendFile(path.resolve(__dirname, '/client/build', 'index.html'));
